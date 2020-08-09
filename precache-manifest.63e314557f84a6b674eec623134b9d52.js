c3ba56d4127c6b3014b58725c7a881cfd944293d
umluizlima <umluizlima@gmail.com>
2020-08-08 23:23:24 -0300

    Updates

---
+++ precache-manifest.63e314557f84a6b674eec623134b9d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeda63e8d3cbb6964dc0bc0398039218",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "0877e83d2d7cd1bc0b00",
+    "url": "/pomodoro/static/css/main.325c9b69.chunk.css"
+  },
+  {
+    "revision": "6257dabb0a9096c83301",
+    "url": "/pomodoro/static/js/2.7ba94a23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.7ba94a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0877e83d2d7cd1bc0b00",
+    "url": "/pomodoro/static/js/main.bd83d150.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
